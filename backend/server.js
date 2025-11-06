a6ffdbda139f39085d1c347228df20f95b2e1368
Reizinger Barnabás <rbarnabas15@gmail.com>
2025-11-06 22:35:46 +0100

    11.06.v2

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,22 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-//send the following question
-app.get('/api/question', (req, res) => {
-  res.json([
-    { id: 1, q: 'Mikor született II.András?', a1: '1232', a2: '1177', a3: '1209' },
-    { id: 2, q: 'Melyik évszázadban élt IV.Béla?', a1: '11', a2: '12', a3: '13' },
-    { id: 3, q: 'Mikor írták alá az Aranybullát?', a1: '1222', a2: '1165', a3: '1308' }
-  ]);
+//send the next question
+const questions = [
+  { id: 1, q: 'Mikor született II.András?', a1: '1232', a2: '1177', a3: '1209' },
+  { id: 2, q: 'Melyik évszázadban élt IV.Béla?', a1: '11', a2: '12', a3: '13' },
+  { id: 3, q: 'Mikor írták alá az Aranybullát?', a1: '1222', a2: '1165', a3: '1308' }
+];
+
+app.get('/api/question/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const question = questions.find(u => u.id === id);
+
+  if (question) {
+    res.json(question);
+  } else {
+    res.status(404).json({ error: 'A kérdés nem található' });
+  }
 });
 
 //get the answer
@@ -23,12 +32,12 @@ app.post('/api/answer', (req, res) => {
   if (data.answer === 2) {
     res.json({
     received: true,
-    message: `Your answer was correct:`
+    message: `A válasz helyes:`
   });
   } else {
     res.json({
       reveived: true,
-      message: 'Your answer was wrong'
+      message: 'A válasz helytelen'
     })
   }
 });
